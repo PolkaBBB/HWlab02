087956fa3f00c4d5e50b3f330622a4c457fc4a70
PolkaBBB <61475184+PolkaBBB@users.noreply.github.com>
2020-05-27 23:55:28 +0300

    Create hello_world.cpp

---
+++ hello_world.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+
+int
+main(int argc, char** argv)
+{
+ string name;  // User n.
+ cin >> name;  // Ввод данных
+ std::cout << "Hello world from " << name << std::endl;
+}
